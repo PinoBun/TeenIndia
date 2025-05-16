e5a50d901397a0bcd0aeb3cd0f8585a3b15bd7ca
Pino <amritkys@gmail.com>
2025-05-16 22:37:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: '/TeenIndia/',  // ← put your repo name here (case-sensitive)
-  plugins: [react()],
+  base: "/TeenIndia/",
+  plugins: [react()]
 })
